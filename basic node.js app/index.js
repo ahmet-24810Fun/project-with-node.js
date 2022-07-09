2dfbdf1d200cf42749b472a3d74f447885a1bd60
ahmet-24810Fun <dragonsfire20201998@gmail.com>
2022-07-09 18:29:03 +0300

    web app with backend node
    basic full stack app with node js

---
+++ basic node.js app/index.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productname);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRİCE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overwiev.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  // overwiev page
+  if (pathname === "/" || pathname === "/overwiev") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+    // product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output);
+    //api
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+
+    res.end(data);
+    //not found
+  } else {
+    res.end("page not found");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening request on port 8000");
+});
